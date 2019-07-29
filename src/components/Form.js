b1d699420215825c298955d4b019248561e13abc
Serkan Okur <serkan.okur@gmail.com>
2019-07-29 22:28:11 +0200

    removes search button
    removes search button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Form = props => {
-
   return (
-    <div className="container">
-      <p></p>
-      <form onSubmit={props.getRecipe}>
-        <div className="row justify-content-center">
-          <div className="col-sm-12 col-md-4 ">
-            <input className="form-control shadow  mb-3 bg-white rounded" type="text" placeholder="food to search" name="searchName" /></div>
-          <div className="col-sm-12 col-md-2">
-            <button className="btn btn-secondary form-control shadow  mb-3 rounded">Search </button></div>
+    <div className='container'>
+      <p />
+
+      <div className='row justify-content-center'>
+        <div className='col-sm-12 col-md-4 '>
+          <input
+            onChange={props.getRecipe}
+            className='form-control shadow  mb-3 bg-white rounded'
+            type='text'
+            placeholder='food to search'
+            name='searchName'
+          />
         </div>
-      </form>
-      <p></p>
+      </div>
+
+      <p />
     </div>
+  );
+};
 
-  )
-}
-
-export default Form
+export default Form;
